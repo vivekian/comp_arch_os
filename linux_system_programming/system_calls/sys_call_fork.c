5e425ecae6d2f59cf37cce1f01f639f935c40acf
vivekian <vivekaseeja@me.com>
2016-05-16 14:52:50 -0700

    process creation: further updates to how fork() call is used

--- linux_system_programming/system_calls/sys_call_fork.c
+++ linux_system_programming/system_calls/sys_call_fork.c
@@ -1,3 +1,6 @@
+#include <sys/stat.h> 
+#include <fcntl.h> 
+#include <sys/wait.h> 
 #include <stdio.h>
 #include <unistd.h> 
 
@@ -14,26 +17,42 @@
 //
 // which executes first after fork()? 
 //  - this is indeterministic as to if parent or child process will execute first. 
+//  - in Linux, there is generally a kernel policy on which to run first - in general this is parent. 
+//  - but kernel devs can go with child first too and there is never 100% guarantee on what should be picked. 
+//  - /proc/sys/kernel/sched_child_runs_first file can be modified to update this behavior. 
 //
-//
+// file descriptors between child and parent process are shared even after fork() is called. 
+// e.g. if a child process executes and changes file attribute for an open file, then it will 
+// be reflected on the parent process. 
 
 int main() 
 {
-    int istack = 22; 
+    int istack = 22;
+    char template[] = "/tmp/testXXXXXX"; 
+
+    setbuf(stdout, NULL); 
+    int fd = mkstemp(template); 
+
+    printf("File offset before fork(): %lld\n", (long long) lseek(fd, 0, SEEK_CUR));
 
     pid_t pid = fork();
    
     if (pid == 0) { 
-        istack *= 3; 
-        printf("child process\n"); 
+        istack *= 3;
+        lseek(fd, 100, SEEK_SET); 
+        printf("child process, istack=%d\n", istack); 
+        _exit(); 
     }
     else if (pid == -1) 
         printf("error\n"); 
     else { 
-        printf("parent process\n"); 
+        // stack data is different between child and parent process 
+        printf("parent process, istack=%d\n", istack); 
+        // wait for child process to complete execution. 
+        wait(NULL);
+        printf("File offset after fork() in parent: %lld\n", (long long) lseek(fd, 0, SEEK_CUR));
     } 
 
-    printf("%u\n", istack); 
 
     return 0; 
 } 
