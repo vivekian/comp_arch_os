6e1d0c6af870fe58614f4266ec79d7578125bb05
vivekian <vivekaseeja@me.com>
2017-08-01 08:09:37 -0700

    file descriptors in fork/exec

--- operating_systems/process/fork_example.c
+++ operating_systems/process/fork_example.c
@@ -1,9 +1,12 @@
+#include <fcntl.h> 
 #include <stdio.h> 
 #include <stdlib.h>
 #include <unistd.h> 
 
 int main()
 {
+   int fd = open("a.txt", O_WRONLY | O_CREAT, 0644); 
+   int x = 23; 
    int rc = fork(); 
 
    if (rc < 0) { 
@@ -11,10 +14,20 @@ int main()
        exit(1); 
    } 
    else if (rc == 0) { 
+       x = 42;
+       char buffer[6] = "child"; 
+       write(fd, buffer, 6);  
        printf("child process: %d\n", getpid()); 
+       printf("value of x: %d\n", x); 
+       close(fd); 
    }
    else { 
-       printf("parent process: %d\n", getpid()); 
+       char buffer[7] = "parent"; 
+       write(fd, buffer, 7); 
+       int wc = wait(NULL);
+       printf("parent process: %d, child: %d, %d\n", getpid(), rc, wc); 
+       printf("value of x: %d\n", x); 
+       close(fd); 
    }
 
    return 0; 
